9938778c35dfd64c455e3e7e4202fd0a69f1c4f6
Starting Ground (Milan Boskovic) <startingground.com@gmail.com>
2022-01-17 13:23:01 +0100

    Add Hilt dependencies

--- app/src/main/java/com/startingground/cognebus/MainActivity.kt
+++ app/src/main/java/com/startingground/cognebus/MainActivity.kt
@@ -3,7 +3,9 @@ package com.startingground.cognebus
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
